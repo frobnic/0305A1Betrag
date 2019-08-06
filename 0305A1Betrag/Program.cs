fe92667bf5f53cc85514ca711ef9e4d5523a19f1
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 10:33:07 +0200

    code inserted

--- 0305A1Betrag/Program.cs
+++ 0305A1Betrag/Program.cs
@@ -10,6 +10,18 @@ namespace _0305A1Betrag
     {
         static void Main(string[] args)
         {
+            decimal z, wert=0;
+
+            Console.Write("Bitte eingeben Sie eine Zahl: ");
+            if (decimal.TryParse(Console.ReadLine(), out z)){
+                if (z < 0)
+                    wert = -z;
+                else if (z == 0)
+                    wert = 0;
+                else
+                    wert = z;
+                Console.WriteLine("Der Betrag von |{0}| ist {1}", z, wert);
+            }
         }
     }
 }
